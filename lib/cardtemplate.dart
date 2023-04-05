ee7cb811ff62cc378f9a936c59e9239216452331
Adepetu Alex <alexadepetu@gmail.com>
2023-04-05 15:58:13 +0100

    commit

--- lib/cardtemplate.dart
+++ lib/cardtemplate.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_typing_uninitialized_variables
+// ignore_for_file: prefer_typing_uninitialized_variables, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -15,13 +15,23 @@ class CardTemplate extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: Container(
+          padding: EdgeInsets.all(5),
           color: Colors.white,
           height: 150,
           width: 100,
-          child: const Center(
-            child: Text(
-              "🖤",
-              style: TextStyle(fontSize: 30),
+          child: Center(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "10",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ),
---
+++ lib/suits.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+Widget heart() {
+  return Text(
+    '♥',
+    style: TextStyle(color: Colors.red[800], fontSize: 20),
+  );
+}
+
+Widget diamond() {
+  return Text(
+    '♦',
+    style: TextStyle(color: Colors.red[800], fontSize: 20),
+  );
+}
+
+Widget spdae() {
+  return Text(
+    '♠',
+    style: TextStyle(color: Colors.black, fontSize: 20),
+  );
+}
+
+Widget clover() {
+  return Text(
+    '♣',
+    style: TextStyle(color: Colors.black, fontSize: 20),
+  );
+}
